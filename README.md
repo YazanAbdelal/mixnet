0f4f78fc3bcde524902d235e97f15b4508a49e01
Yazan Abdelal <yazan.r.abdelal@gmail.com>
2026-07-04 20:15:01 +0300

    added certificate generation for mTLS in script.sh

--- README.md
+++ README.md
@@ -4,3 +4,42 @@ A mixnet implementation in Go using Docker compose.
 ## How to Start the Mixnet:
 Run the script: `./script.sh` and input the number of mixnet servers and clients. This will create the keys and docker-compose.yml file.
 Then run the mixnet using Docker compose: `docker compose up -d`
+
+## For mTLS Encryption:
+#### First we create a **Certificate Authority** (CA):
+generate a RSA key of length 2048 and save it in ca.key
+
+this key is private and is used to sign cerficates.
+
+``` sh
+openssl genrsa -out ca.key 2048
+```
+___
+generate a self-signed (x509) certificate using the private key in ca.key, and save the certificate in ca.crt. And set the identity of the CA as mixnet-CA.
+
+`-nodes` means that the cerificate is not password-protected.
+
+this certificate is public and is used to verify signatures.
+
+```sh
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 -out ca.crt -subj "/CN=mixnet-CA" 
+```
+___
+#### Then we create a certificate for each of the nodes:
+>here NODE_NAME should match the Docker service name.
+
+generate a new RSA key.
+```sh
+openssl genrsa -out NODE_NAME.key 2048
+```
+___
+generate a new **Certificate Signing Request (CSR)**: The node sends its public key (derived from the private key) and its identity and signs them using its private key.
+```sh
+openssl req -new -key NODE_NAME.key -out NODE_NAME.csr -subj "/CN=NODE_NAME"
+```
+___
+CA signs certificate using its private key: Takes in a CSR as input (`-req`), specifies the certificate holder (`-in NODE_NAME.csr`), and the Certificate Authority (CA) (`-CAkey ca.key`) and creates a serial number (not important) (`-CAcreateserial`).
+```sh
+openssl x509 -req -in NODE_NAME.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out NODE_NAME.crt -days 365 -sha256
+```
+___
--- scripts/script.sh
+++ scripts/script.sh
@@ -10,6 +10,28 @@ read -p "Number of clients: " num_clients
 # run the keygen binary from the root directory to output to the ./keys folder
 go run ./keygen/cmd -servers "$num_nodes" -clients "$num_clients"
 
+# generate CA certificate
+mkdir -p certs
+openssl genrsa -out certs/ca.key 2048
+openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 365 -out certs/ca.crt -subj "/CN=mixnet-CA" 
+
+# generate TLS certificates for all nodes (mTLS)
+for ((i = 1; i <= num_nodes; i++)); do
+  name="mixnode-$i"
+  openssl genrsa -out "certs/$name.key" 2048
+  openssl req -new -key "certs/$name.key" -out "certs/$name.csr" -subj "/CN=$name"
+  openssl x509 -req -in "certs/$name.csr" -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out "certs/$name.crt" -days 365 -sha256
+  rm "certs/$name.csr"
+done
+
+for ((i = 1; i <= num_clients; i++)); do
+  name="client-$i"
+  openssl genrsa -out "certs/$name.key" 2048
+  openssl req -new -key "certs/$name.key" -out "certs/$name.csr" -subj "/CN=$name"
+  openssl x509 -req -in "certs/$name.csr" -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out "certs/$name.crt" -days 365 -sha256
+  rm "certs/$name.csr"
+done
+
 # generate docker-compose.yml based on the requested topology
 cat > "$script_dir/docker-compose.yml" <<COMPOSE_EOF
 version: '3.8'
@@ -29,6 +51,9 @@ for ((i = 1; i <= num_nodes; i++)); do
     volumes:
       - ./keys/server_${i}_private.pem:/etc/mixnet/keys/private.pem:ro
       - ./keys/public:/etc/mixnet/keys/public:ro
+      - ./certs/mixnode-${i}.crt:/etc/mixnet/certs/tls.crt:ro
+      - ./certs/mixnode-${i}.key:/etc/mixnet/certs/tls.key:ro
+      - ./certs/ca.crt:/etc/mixnet/certs/ca.crt:ro
     ports:
       - "500${i}:500${i}"
     networks:
@@ -46,6 +71,9 @@ for ((i = 1; i <= num_clients; i++)); do
     volumes:
       - ./keys/client_${i}_private.pem:/etc/mixnet/keys/private.pem:ro
       - ./keys/public:/etc/mixnet/keys/public:ro
+      - ./certs/client-${i}.crt:/etc/mixnet/certs/tls.crt:ro
+      - ./certs/client-${i}.key:/etc/mixnet/certs/tls.key:ro
+      - ./certs/ca.crt:/etc/mixnet/certs/ca.crt:ro
     depends_on:
     networks:
       - mixnet
