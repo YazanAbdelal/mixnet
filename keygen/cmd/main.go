6dc8443db6db5264f647893fc33a4435bd28de9e
Yazan Abdelal <yazan.r.abdelal@gmail.com>
2026-07-08 03:24:58 +0300

    refactored the script.sh file

--- keygen/cmd/main.go
+++ keygen/cmd/main.go
@@ -23,8 +23,8 @@ func main() {
 	keysFolder := "keys"
 	publicKeysFolder := keysFolder + "/public"
 	for i := range *clientCount {
-		privateKeyPath := "client_" + strconv.Itoa(i+1) + "_private.pem"
-		publicKetPath := "client_" + strconv.Itoa(i+1) + "_public.pem"
+		privateKeyPath := "client-" + strconv.Itoa(i+1) + "-private.pem"
+		publicKetPath := "client-" + strconv.Itoa(i+1) + "-public.pem"
 		private, public, err := keygen.GenerateKeys(KeySize)
 		if err != nil {
 			fmt.Print("Error generating keys: " + err.Error())
@@ -43,8 +43,8 @@ func main() {
 	}
 
 	for i := range *serverCount {
-		privateKeyPath := "server_" + strconv.Itoa(i+1) + "_private.pem"
-		publicKetPath := "server_" + strconv.Itoa(i+1) + "_public.pem"
+		privateKeyPath := "server-" + strconv.Itoa(i+1) + "-private.pem"
+		publicKetPath := "server-" + strconv.Itoa(i+1) + "-public.pem"
 		private, public, err := keygen.GenerateKeys(KeySize)
 		if err != nil {
 			fmt.Print("Error generating keys: " + err.Error())
--- scripts/script.sh
+++ scripts/script.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 set -euo pipefail
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+generate_ca_cert() {
+	mkdir -p certs
+	openssl genrsa -out certs/ca.key 2048
+	openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 365 \
+		-out certs/ca.crt -subj "/CN=mixnet-CA"
+}
 
-# get number of mix servers and clients in the mixnet
-read -p "Number of mix nodes: " num_nodes
-read -p "Number of clients: " num_clients
+generate_tls_cert() {
+	local name="$1"
 
-# run the keygen binary from the root directory to output to the ./keys folder
-go run ./keygen/cmd -servers "$num_nodes" -clients "$num_clients"
+	openssl genrsa -out "certs/$name.key" 2048
+	openssl req -new -key "certs/$name.key" -out "certs/$name.csr" \
+		-subj "/CN=$name" \
+		-addext "subjectAltName=DNS:$name"
+	openssl x509 -req -in "certs/$name.csr" \
+		-CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial \
+		-out "certs/$name.crt" -days 365 -sha256 -copy_extensions copy
+	rm "certs/$name.csr"
+}
 
-# generate CA certificate
-mkdir -p certs
-openssl genrsa -out certs/ca.key 2048
-openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 365 -out certs/ca.crt -subj "/CN=mixnet-CA" 
+generate_tls_certs() {
+	generate_ca_cert
 
-# generate TLS certificates for all nodes (mTLS)
-for ((i = 1; i <= num_nodes; i++)); do
-  name="mixnode-$i"
-  openssl genrsa -out "certs/$name.key" 2048
-  openssl req -new -key "certs/$name.key" -out "certs/$name.csr" -subj "/CN=$name" -addext "subjectAltName=DNS:$name,DNS:$name" # -addext is for SAN
-  openssl x509 -req -in "certs/$name.csr" -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out "certs/$name.crt" -days 365 -sha256 -copy_extensions copy # -copy_extentions copies the SAN
-  rm "certs/$name.csr"
-done
+	for ((i = 1; i <= num_nodes; i++)); do
+		generate_tls_cert "mixnode-$i"
+	done
 
-for ((i = 1; i <= num_clients; i++)); do
-  name="client-$i"
-  openssl genrsa -out "certs/$name.key" 2048
-  openssl req -new -key "certs/$name.key" -out "certs/$name.csr" -subj "/CN=$name" -addext "subjectAltName=DNS:$name,DNS:$name"
-  openssl x509 -req -in "certs/$name.csr" -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out "certs/$name.crt" -days 365 -sha256 -copy_extensions copy
-  rm "certs/$name.csr"
-done
+	for ((i = 1; i <= num_clients; i++)); do
+		generate_tls_cert "client-$i"
+	done
+}
 
-# generate docker-compose.yml based on the requested topology
-cat > docker-compose.yml <<COMPOSE_EOF
+generate_compose_file() {
+	cat > docker-compose.yml <<COMPOSE_EOF
 
 networks:
   mixnet:
@@ -41,15 +42,15 @@ networks:
 services:
 COMPOSE_EOF
 
-for ((i = 1; i <= num_nodes; i++)); do
-  cat >> docker-compose.yml <<COMPOSE_EOF
+	for ((i = 1; i <= num_nodes; i++)); do
+		cat >> docker-compose.yml <<COMPOSE_EOF
   mixnode-$i:
     container_name: server-${i}
     build:
       context: .
       dockerfile: server/Dockerfile
     volumes:
-      - ./keys/server_${i}_private.pem:/etc/mixnet/keys/private.pem:ro
+      - ./keys/server-${i}-private.pem:/etc/mixnet/keys/private.pem:ro
       - ./keys/public:/etc/mixnet/keys/public:ro
       - ./certs/mixnode-${i}.crt:/etc/mixnet/certs/tls.crt:ro
       - ./certs/mixnode-${i}.key:/etc/mixnet/certs/tls.key:ro
@@ -60,11 +61,11 @@ for ((i = 1; i <= num_nodes; i++)); do
       - mixnet
 
 COMPOSE_EOF
-done
+	done
 
-for ((i = 1; i <= num_clients; i++)); do
-  clientPort=$((50050 + i))
-  cat >> docker-compose.yml <<COMPOSE_EOF
+	for ((i = 1; i <= num_clients; i++)); do
+		local clientPort=$((50050 + i))
+		cat >> docker-compose.yml <<COMPOSE_EOF
   client-$i:
     container_name: client-${i}
     build:
@@ -74,19 +75,29 @@ for ((i = 1; i <= num_clients; i++)); do
     stdin_open: true
     tty: true
     volumes:
-      - ./keys/client_${i}_private.pem:/etc/mixnet/keys/private.pem:ro
+      - ./keys/client-${i}-private.pem:/etc/mixnet/keys/private.pem:ro
       - ./keys/public:/etc/mixnet/keys/public:ro
       - ./certs/client-${i}.crt:/etc/mixnet/certs/tls.crt:ro
       - ./certs/client-${i}.key:/etc/mixnet/certs/tls.key:ro
       - ./certs/ca.crt:/etc/mixnet/certs/ca.crt:ro
     depends_on:
 COMPOSE_EOF
-  for ((j = 1; j <= num_nodes; j++)); do
-    echo "      - mixnode-$j" >> docker-compose.yml
-  done
-  cat >> docker-compose.yml <<COMPOSE_EOF
+		for ((j = 1; j <= num_nodes; j++)); do
+			echo "      - mixnode-$j" >> docker-compose.yml
+		done
+		cat >> docker-compose.yml <<COMPOSE_EOF
     networks:
       - mixnet
 
 COMPOSE_EOF
-done
+	done
+}
+
+# main flow
+read -p "Number of mix nodes: " num_nodes
+read -p "Number of clients: " num_clients
+
+go run ./keygen/cmd -servers "$num_nodes" -clients "$num_clients"
+
+generate_tls_certs
+generate_compose_file
